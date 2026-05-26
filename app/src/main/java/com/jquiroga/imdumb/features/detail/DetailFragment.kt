73ddcd425b417f5543cef226fd79d6cdc589a241
Jamer Quiroga <jamerquiroga@gmail.com>
2026-05-26 12:48:11 -0500

    add navigation graph and create home and detail fragments

---
+++ app/src/main/java/com/jquiroga/imdumb/features/detail/DetailFragment.kt
@@ -0,0 +1,18 @@
+package com.jquiroga.imdumb.features.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jquiroga.imdumb.R
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+}
---
+++ app/src/main/java/com/jquiroga/imdumb/features/main/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.jquiroga.imdumb.features.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.jquiroga.imdumb.R
+import com.jquiroga.imdumb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(viewBinding.root)
+        configNavigation()
+    }
+
+    private fun configNavigation() {
+        setSupportActionBar(viewBinding.toolbarMain)
+
+        val appBarConfiguration = AppBarConfiguration
+            .Builder(R.id.homeFragment)
+            .build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
